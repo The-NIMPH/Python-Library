2ec102ad0e20ec02420aa79b66c01e8059b6d2c5
Foxie-404 <2038834945@qq.com>
2025-09-21 16:14:22 +0800

    Correct version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('./README.md', encoding = 'utf-8') as f:
 
 setup(
     name = "PyEasyUtils",
-    version = '0.3.0',
+    version = '0.3.1',
     description = 'A simple python util library',
     long_description = LongDescription,
     long_description_content_type = 'text/markdown',
